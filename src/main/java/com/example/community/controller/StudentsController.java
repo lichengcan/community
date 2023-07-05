20641e6c6ef88556dd7529afe8fbfc50c6ee7479
lichengcan <li929825769@163.com>
2023-07-05 09:43:46 +0800

    springboot整合mybatis-plus

--- src/main/java/com/example/community/controller/StudentsController.java
+++ src/main/java/com/example/community/controller/StudentsController.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.community.model.entity.Students;
 import com.example.community.mapper.StudentsMapper;
+import com.example.community.service.StudentsService;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,11 +20,11 @@ import org.springframework.web.bind.annotation.*;
 public class StudentsController {
 
     @Autowired
-    StudentsMapper studentsMapper;
+    StudentsService studentsService;
 
     @PostMapping("/test")
     public void test(@RequestBody Students students) {
-        studentsMapper.insert(students);
+        studentsService.insert(students);
     }
 
     /**
@@ -32,7 +33,7 @@ public class StudentsController {
     @GetMapping("/testPage")
     public IPage<Students> testPage() {
         Page<Students> page = new Page<>(1, 2);
-        IPage<Students> userIPage = studentsMapper.selectPage(page, new QueryWrapper<Students>()
+        IPage<Students> userIPage = studentsService.selectPage(page, new QueryWrapper<Students>()
                 .eq("name", "name_a2frd"));
         return userIPage;
     }
--- src/main/java/com/example/community/service/impl/StudentsServiceImpl.java
+++ src/main/java/com/example/community/service/impl/StudentsServiceImpl.java
@@ -1,16 +1,35 @@
-//package com.example.community.service.impl;
-//
-//import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-//import com.example.community.model.entity.Students;
-//import com.example.community.mapper.StudentsMapper;
-//import com.example.community.service.StudentsService;
-//import org.springframework.stereotype.Service;
-//
-///**
-// * @author: lichengcan
-// * @date: 2023-07-04 17:28
-// * @description
-// **/
-//@Service
-//public class StudentsServiceImpl extends ServiceImpl<StudentsMapper, Students> implements StudentsService {
-//}
+package com.example.community.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.community.model.entity.Students;
+import com.example.community.mapper.StudentsMapper;
+import com.example.community.service.StudentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: lichengcan
+ * @date: 2023-07-04 17:28
+ * @description
+ **/
+@Service
+public class StudentsServiceImpl extends ServiceImpl<StudentsMapper, Students> implements StudentsService {
+
+    @Autowired
+    StudentsMapper studentsMapper;
+
+
+    @Override
+    public IPage<Students> selectPage(Page<Students> page, QueryWrapper<Students> eq) {
+        return studentsMapper.selectPage(page, new QueryWrapper<Students>()
+                .eq("name", "name_a2frd"));
+    }
+
+    @Override
+    public void insert(Students students) {
+        studentsMapper.insert(students);
+    }
+}
