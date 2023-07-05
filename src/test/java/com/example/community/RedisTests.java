700060b589d6b79684afc395e50f908264e4e70e
lichengcan <li929825769@163.com>
2023-07-05 10:18:56 +0800

    0702 springboot 3 整合Redis

---
+++ src/test/java/com/example/community/RedisTests.java
@@ -0,0 +1,30 @@
+package com.example.community;
+
+import com.example.community.util.RedisTemplateUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.DataType;
+
+@SpringBootTest
+class RedisTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Autowired
+    RedisTemplateUtil redisTemplateUtil;
+
+    @Test
+    public void testAdd() {
+        redisTemplateUtil.lSet("name", "lichengcan");
+    }
+
+    @Test
+    public void testQuery() {
+        final DataType type = redisTemplateUtil.getType("name");
+        System.out.println("type = " + type);
+        System.out.println(redisTemplateUtil.lGet("name",0,1));
+    }
+}
