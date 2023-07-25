aeca99627f2b7f127882f12de0089382bfde83fc
lichengcan <li929825769@163.com>
2023-07-25 14:10:56 +0800

    配置文件修改

--- src/test/java/com/example/community/mongDB/MongDbApplicationTests.java
+++ src/test/java/com/example/community/mongDB/MongDbApplicationTests.java
@@ -63,7 +63,7 @@ class MongDbApplicationTests {
                 new User(2, "今天吃什么", 77777, new Date()),
                 new User(3, "好饿", 12345, new Date())
         );
-        mongoTemplate.insert(users, User.class);
+//        mongoTemplate.insert(users, User.class);
     }
 
     /**
