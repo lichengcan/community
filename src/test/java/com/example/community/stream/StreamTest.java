9f12f07c7007b53ae82d1cc0c563ffbb7b344f6a
lichengcan <li929825769@163.com>
2023-07-28 13:45:05 +0800

    stream流练习

--- src/test/java/com/example/community/stream/StreamTest.java
+++ src/test/java/com/example/community/stream/StreamTest.java
@@ -27,7 +27,8 @@ public class StreamTest {
 //        test2();
 //        test3();
 //        test5();
-        test6();
+//        test6();
+        test7();
     }
 
 
@@ -137,4 +138,35 @@ public class StreamTest {
         List<Actor> actors = concat.map(x -> new Actor(x)).collect(Collectors.toList());
         actors.forEach(System.out::println);
     }
+
+    public static void test7() {
+        String[] arr = {"河北省", "山西省", "吉林省", "辽宁省",
+                "黑龙江省", "陕西省", "甘肃省", "青海省", "山东省",
+                "福建省", "浙江省", "台湾省", "河南省", "湖北省",
+                "湖南省", "江西省", "江苏省", "安徽省", "广东省",
+                "海南省", "四川省", "贵州省", "云南省", "北京市",
+                "天津市", "上海市", "重庆市", "内蒙古自治区",
+                "新疆维吾尔自治区", "宁夏回族自治区", "广西壮族自治区",
+                "西藏自治区", "北香港特别行政区", "澳门特别行政区"};
+
+//        我国有34个省级行政区，分别是：
+//        23个省：河北省、山西省、吉林省、辽宁省、黑龙江省、陕西省、甘肃省、青海省、山东省、福建省、浙江省、台湾省、河南省、湖北省、湖南省、江西省、江苏省、安徽省、广东省、海南省、四川省、贵州省、云南省。
+//        4个直辖市：北京市、天津市、上海市、重庆市。 5个自治区：内蒙古自治区、新疆维吾尔自治区、宁夏回族自治区、广西壮族自治区、西藏自治区。
+//        2个特别行政区：香港特别行政区、澳门特别行政区。 请使用流依次完成下列操作：
+//        题目17：统计三个字的省份的个数
+        long count = Arrays.stream(arr)
+                .filter(x -> x.length() == 3).count();
+//        题目18：统计名字中包含方位名词的省份（东西南北）的个数
+        final long count1 = Arrays.stream(arr)
+                .filter(x -> x.contains("东") || x.contains("西") || x.contains("南") || x.contains("北")).count();
+//        题目19：打印名字中包含方位名词的普通省份（非自治区直辖市特别行政区）的名字
+        Arrays.stream(arr)
+                .filter(x -> x.contains("东") || x.contains("西") || x.contains("南") || x.contains("北"))
+                .filter(x -> !x.contains("自治区直辖市特别行政区")).forEach(System.out::println);
+//        题目20：将所有的特殊省份（自治区直辖市特别行政区）提取出来并放到新数组中
+        List<String> collect = Arrays.stream(arr)
+                .filter(x -> x.contains("自治区") || x.contains("特别行政区")).collect(Collectors.toList());
+        System.out.println("collect = " + collect);
+    }
+
 }
