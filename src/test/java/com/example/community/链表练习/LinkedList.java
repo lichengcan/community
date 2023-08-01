1b483d5194c1e5e301362665d6ca375c0642ae3d
lichengcan <li929825769@163.com>
2023-08-01 15:07:18 +0800

    从链表中删除值为val的节点

--- src/test/java/com/example/community/链表练习/LinkedList.java
+++ src/test/java/com/example/community/链表练习/LinkedList.java
@@ -56,6 +56,47 @@ public class LinkedList {
     }
 
 
+    /**
+     * 从链表中删除值为val的节点
+     *
+     * @param headNode 链表头节点
+     * @param val 要删除的节点值
+     * @return 删除后的链表头节点
+     */
+    public static Node removeElements(Node headNode, int val) {
+        //判断头节点是否为空
+        if (headNode == null) {
+            return null;
+        }
+        //前驱节点
+        Node prev = headNode;
+        //当前节点
+        Node cur = headNode.next;
+        //当前节点不为空：
+        while(cur != null) {
+            //判断当前节点是否为需要删除的节点
+            if (cur.data == val) {
+                //前驱节点指向当前节点的下一个节点，跳过当前节点
+                prev.next = cur.next;
+                //当前节点指向下一个节点，继续进行遍历链表
+                cur = cur.next;
+            } else {
+                //前驱节点指向当前节点
+                prev = cur;
+                //当前节点指向下一节点
+                cur = cur.next;
+            }
+        }
+        //只包含头节点的情况
+        if (headNode.data == val) {
+            //头节点指向下一个节点，下一个节点是空
+            headNode = headNode.next;
+        }
+        // 返回删除后的链表头节点
+        return headNode;
+    }
+
+
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
@@ -63,5 +104,10 @@ public class LinkedList {
         list.addNode(2);
         list.addNode(3);
         list.printList(); // Output: 1 2 3
+        Node node = removeElements(list.head, 1);
+        while (node!= null) {
+            System.out.println("node = " + node.data);
+            node=node.next;
+        }
     }
 }
