1a2d98da8d9f9ce9487dc1096ba2351117247ed1
lichengcan <li929825769@163.com>
2023-08-02 10:06:48 +0800

    buildTreeV2

--- src/test/java/com/example/community/stream/TreeStreamTest.java
+++ src/test/java/com/example/community/stream/TreeStreamTest.java
@@ -4,6 +4,7 @@ import com.example.community.model.entity.Tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -23,12 +24,14 @@ public class TreeStreamTest {
         TreeList.add(new Tree("d", "a", "ddd"));
         TreeList.add(new Tree("e", "b", "eee"));
         TreeList.add(new Tree("g", "e", "fff"));
+        TreeList.add(new Tree("i", "x", "iiii"));
         return TreeList;
     }
 
     public static void main(String[] args) {
         //初始化数据
         List<Tree> treeList = createTreeList();
+        buildTreeV2(treeList);
         //获取根节点
         List<Tree> trees = getRoot(treeList);
         System.out.println("trees = " + trees);
@@ -37,12 +40,12 @@ public class TreeStreamTest {
     public static List<Tree> getRoot(List<Tree> trees) {
         List<Tree> roots = trees.stream().filter(x -> x.getParentId().equals("0")).collect(Collectors.toList());
         //构建树形结构
-        roots.stream().forEach(root -> buildTree(root, trees));
+        roots.stream().forEach(root -> buildTreeV1(root, trees));
         //只保留根节点对象
         return trees.stream().filter(tree -> tree.getParentId().equals("0")).collect(Collectors.toList());
     }
 
-    public static void buildTree(Tree root, List<Tree> trees) {
+    public static void buildTreeV1(Tree root, List<Tree> trees) {
         trees.stream()
                 .forEach(tree -> {
                     if (tree.getParentId().equals(root.getId())) {
@@ -51,8 +54,37 @@ public class TreeStreamTest {
                         }
                         root.getChild().add(tree);
                         //把当前节点和整个数组重新
-                        buildTree(tree, trees);
+                        buildTreeV1(tree, trees);
                     }
                 });
+    }
+
+    public static List<Tree> buildTreeV2(List<Tree> trees) {
+        //将List转换成map,id作为key可以更方便的拿到父节点、子节点，减少了重复扫描整个列表，降低了时间复杂度
+        Map<String, Tree> treeMap = trees.stream().collect(Collectors.toMap(Tree::getId, t -> t));
+        //开始构建树
+        List<Tree> roots = new ArrayList<>();
+        trees.stream().forEach(tree -> {
+            //当前节点的爸爸id
+            String parentId = tree.getParentId();
+            //是不是老大
+            if ("0".equals(parentId)) {
+                roots.add(tree);
+            } else {
+                //不是老大
+                //拿到爸爸
+                Tree parent = treeMap.get(parentId);
+                //爸爸没有儿子
+                if (parent != null) {
+                    //初始化空儿子
+                    if (parent.getChild() == null) {
+                        parent.setChild(new ArrayList<>());
+                    }
+                    //给他一个亲生儿子
+                    parent.getChild().add(tree);
+                }
+            }
+        });
+        return roots;
     }
 }
